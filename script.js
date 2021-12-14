b83c2362d3702c451666470ba9d030341569e3e7
danielmuthama <daniel.muthama@student.moringaschool.com>
2021-12-14 12:39:38 +0300

    testing functionality

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ function nameGenerator() {
     let male= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     
-    let dob = Number(document.getElementById("day").value);
-    let mob = Number(document.getElementById("month").value);
-    let yob = Number(document.getElementById("year").value);
+    let dob = Number(document.getElementById("d").value);
+    let mob = Number(document.getElementById("m").value);
+    let yob = (document.getElementById("y").value);
     let sex = document.getElementsByName("gender");
 
 
     function validatingdob() {
 
-        if (month === 2 && yob % 4 === 0) {
-            if (day < 1 || day > 28) {
+        if (mod === 2 && Number(yob) % 4 === 0) {
+            if (dob < 1 || dob > 28) {
                 alert("This isn't a leap year enter a valid date between 1 and 28");
-            } else if (day < 1 && day > 29) {
+            } else if (dob < 1 && dob > 29) {
                 alert("This is a leap year enter a valid date between 1 and 29");
             } else {
                 return true;
             }
-        } else if (day < 1 || day > 31) {
+        } else if (dob < 1 || dob > 31) {
             alert("Enter a valid date between 1 and 31");
         } else {
             return true;
@@ -35,7 +35,7 @@ function nameGenerator() {
         }
     }
     function validatingyob() {
-        if (yob < 0001 || yob > 2021 && yob.length != 4) {
+        if (Number(yob) < 0001 || Number(yob) > 2021 && yob.length != 4) {
             alert("Enter a four digit number between 0001 and 2021");
         } else {
             return true;
@@ -55,69 +55,70 @@ function nameGenerator() {
     let checkedgender = gender();
 
     let dofWeekNumber = Math.floor((((Number(yob.slice(0, 2)) / 4) - 2 * Number(yob.slice(0, 2)) - 1) +
-    ((5 * Number(yob.slice(2, 4)) / 4)) + ((26 * (mob + 1) / 10)) + dob) % 7);
-
+        ((5 * Number(yob.slice(2, 4)) / 4)) + ((26 * (mob + 1) / 10)) + dob) % 7);
+   
     if (checkedgender == "male" && validateddob && validatedmob && validatedyob) {
        
         switch (dofWeekNumber) {
             case 1:
-                document.getElementById("name").innerHTML = " " + male[0];
-                document.getElementById("day").innerHTML = " " + day[0];
+                document.getElementById("input-name").innerHTML = " " + male[0];
+                document.getElementById("input-day").innerHTML = " " + day[0];
                 return false;
             case 2:
-                document.getElementById("ame").innerHTML = " " + male[1];
-                document.getElementById("day").innerHTML = " " + day[1];
+                document.getElementById("input-name").innerHTML = " " + male[1];
+                document.getElementById("input-day").innerHTML = " " + day[1];
                 return false;
             case 3:
-                document.getElementById("name").innerHTML = " " + male[2];
-                document.getElementById("day").innerHTML = " " + day[2];
+                document.getElementById("input-name").innerHTML = " " + male[2];
+                document.getElementById("input-day").innerHTML = " " + day[2];
                 return false;
             case 4:
-                document.getElementById("name").innerHTML = " " + male[3];
-                document.getElementById("day").innerHTML = " " + day[3];
+                document.getElementById("input-name").innerHTML = " " + male[3];
+                document.getElementById("input-day").innerHTML = " " + day[3];
                 return false;
             case 5:
-                document.getElementById("name").innerHTML = " " + male[4];
-                document.getElementById("day").innerHTML = " " + day[4];
+                document.getElementById("input-name").innerHTML = " " + male[4];
+                document.getElementById("input-day").innerHTML = " " + day[4];
                 return false;
             case 6:
-                document.getElementById("name").innerHTML = " " + male[5];
-                document.getElementById("day").innerHTML = " " + day[5];
+                document.getElementById("input-name").innerHTML = " " + male[5];
+                document.getElementById("input-day").innerHTML = " " + day[5];
                 return false;
             case 7:
-                document.getElementById("name").innerHTML = " " + male[6];
-                document.getElementById("day").innerHTML = " " + day[6];
+                document.getElementById("input-name").innerHTML = " " + male[6];
+                document.getElementById("input-day").innerHTML = " " + day[6];
                 return false;
         }
     } else if (checkedgender == "female" && validateddob && validatedmob && validatedyob) {
-        switch (dayOfWeekNumber) {
+       
+        switch (dofWeekNumber) {
             case 1:
-                document.getElementById("name").innerHTML = " " + female[0];
-                document.getElementById("day").innerHTML = " " + day[0];
+                document.getElementById("input-name").innerHTML = " " + female[0];
+                document.getElementById("input-day").innerHTML = " " + day[0];
                 return false;
             case 2:
-                document.getElementById("name").innerHTML = " " + female[1];
-                document.getElementById("day").innerHTML = " " + day[1];
+                document.getElementById("input-name").innerHTML = " " + female[1];
+                document.getElementById("input-day").innerHTML = " " + day[1];
                 return false;
             case 3:
-                document.getElementById("name").innerHTML = " " + female[2];
-                document.getElementById("day").innerHTML = " " + day[2];
+                document.getElementById("input-name").innerHTML = " " + female[2];
+                document.getElementById("input-day").innerHTML = " " + day[2];
                 return false;
             case 4:
-                document.getElementById("name").innerHTML = " " + female[3];
-                document.getElementById("day").innerHTML = " " + day[3];
+                document.getElementById("input-name").innerHTML = " " + female[3];
+                document.getElementById("input-day").innerHTML = " " + day[3];
                 return false;
             case 5:
-                document.getElementById("name").innerHTML = " " + female[4];
-                document.getElementById("day").innerHTML = " " + day[4];
+                document.getElementById("input-name").innerHTML = " " + female[4];
+                document.getElementById("input-day").innerHTML = " " + day[4];
                 return false;
             case 6:
-                document.getElementById("name").innerHTML = " " + female[5];
-                document.getElementById("day").innerHTML = " " + day[5];
+                document.getElementById("input-name").innerHTML = " " + female[5];
+                document.getElementById("input-day").innerHTML = " " + day[5];
                 return false;
             case 7:
-                document.getElementById("name").innerHTML = " " + female[6];
-                document.getElementById("day").innerHTML = " " + day[6];
+                document.getElementById("input-name").innerHTML = " " + female[6];
+                document.getElementById("input-day").innerHTML = " " + day[6];
                 return false;
         }
     }
